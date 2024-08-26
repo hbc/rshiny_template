66d6b2e231a15d3102e16624e1f2c9230b3b9ee0
Gammerdinger <wjgdinger@gmail.com>
2024-08-26 14:43:00 -0400

    Update app.R

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ library(shiny)
 
 ui <- fluidPage(
   includeCSS("www/style.css"),
-  navbarPage(div("KEGG Pathway Overlap", img(src="HBC_logo.png", width = "40%", height = "40%", align = "right")))
+  navbarPage(div("Insert Title Here", img(src="HBC_logo.png", width = "40%", height = "40%", align = "right")))
 )
 
 server <- function(input, output, session) {
